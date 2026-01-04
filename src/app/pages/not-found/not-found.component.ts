fc366e93a4ef6c546fc1dfea24dbc343a576fba0
kessyy <kather946@gmail.com>
2026-01-04 15:54:23 +0300

    feat: added 404 component

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../../components';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
 export class NotFoundComponent {
+  constructor(private router: Router) {}
 
+  goHome() {
+    this.router.navigate(['/']);
+  }
 }
